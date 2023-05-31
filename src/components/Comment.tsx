0ec0e88f8f9a770a9064290399cdd30e8780034d
GabrielRioo <gabrielpaivario@gmail.com>
2023-05-30 21:22:29 -0300

    Refactoring the JS code to TS code

---
+++ src/components/Comment.tsx
@@ -0,0 +1,46 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar src="https://github.com/Gabrielrioo.png" hasBorder={false}/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Rio</strong>
+                            <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:23'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar Comentário'>
+                            <Trash sise={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+        </div>
+    )
+}
